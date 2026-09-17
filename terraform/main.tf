ce44db4c3248e2522c58e746a77c53c46a806847
BradleyCulley <culleyib@gmail.com>
2026-09-17 14:50:50 -0700

    WIP

--- terraform/main.tf
+++ terraform/main.tf
@@ -47,7 +47,8 @@ resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {
 }
 
 resource "aws_security_group" "rds" {
-  name        = "${var.project_name}-rds-sg"
+  # use name_prefix to avoid duplicate-name errors when the same name already exists
+  name_prefix = "${var.project_name}-rds-sg-"
   description = "Allow PostgreSQL traffic for the blog subscription database."
 
   ingress {
